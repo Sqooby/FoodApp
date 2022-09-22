783e55ba519b264b2c1587c1315dab0b558ff517
Sqooby <michal.basznianin3@gmail.com>
2022-09-22 14:12:03 +0200

    first commit

---
+++ food_app/lib/models/categories.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Category {
+  final String id;
+  final String title;
+  final Color color;
+
+  const Category({
+    this.id = 'c1',
+    this.title = 'tytul',
+    this.color = Colors.orange,
+  });
+}
