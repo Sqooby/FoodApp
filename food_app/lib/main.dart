88b1da5733d932729c6e166cff4efdd452cbbd88
Sqooby <michal.basznianin3@gmail.com>
2023-01-13 17:09:21 +0100

    Category Provider added

--- food_app/lib/main.dart
+++ food_app/lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 
-import './screens/filtres_screen.dart';
-import './screens/tabs_screen.dart';
-import './screens/meal_detail_screen.dart';
-import './screens/categories_meal_screen.dart';
+import 'package:food_app/providers/meals.dart';
+import 'package:provider/provider.dart';
+import './providers/categories.dart';
+
 import './screens/categories_screen.dart';
-import './dummy_data.dart';
-import './models/meal.dart';
 
 void main() => runApp(MyApp());
 
@@ -16,93 +14,98 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  List<Meal> _availableMeal = DUMMY_MEALS;
-  List<Meal> _favoriteMeals = [];
+  // List<Meal> _availableMeal = Provider.of<Meals>(context);
+  // List<Meal> _favoriteMeals = [];
 
-  Map<String, bool> _filters = {
-    'gluten': false,
-    'lactose': false,
-    'vegan': false,
-    'vegeterian': false,
-  };
+  // Map<String, bool> _filters = {
+  //   'gluten': false,
+  //   'lactose': false,
+  //   'vegan': false,
+  //   'vegeterian': false,
+  // };
 
-  void _setFilters(Map<String, bool> filterData) {
-    setState(() {
-      _filters = filterData;
-      _availableMeal = DUMMY_MEALS.where((meal) {
-        if (_filters['gluten'] == true && !meal.isGlutenFree) {
-          return false;
-        }
-        if (_filters['lactose'] == true && !meal.isLactoseFree) {
-          return false;
-        }
-        if (_filters['vegan'] == true && !meal.isVegan) {
-          return false;
-        }
-        if (_filters['vegeterian'] == true && !meal.isVegetarian) {
-          return false;
-        }
-        return true;
-      }).toList();
-    });
-  }
+  // void _setFilters(Map<String, bool> filterData) {
+  //   setState(() {
+  //     _filters = filterData;
+  //     _availableMeal = DUMMY_MEALS.where((meal) {
+  //       if (_filters['gluten'] == true && !meal.isGlutenFree) {
+  //         return false;
+  //       }
+  //       if (_filters['lactose'] == true && !meal.isLactoseFree) {
+  //         return false;
+  //       }
+  //       if (_filters['vegan'] == true && !meal.isVegan) {
+  //         return false;
+  //       }
+  //       if (_filters['vegeterian'] == true && !meal.isVegetarian) {
+  //         return false;
+  //       }
+  //       return true;
+  //     }).toList();
+  //   });
+  // }
 
-  void _toggleFavorite(String mealId) {
-    final existingIndex =
-        _favoriteMeals.indexWhere((meal) => meal.id == mealId);
-    if (existingIndex >= 0) {
-      setState(() {
-        _favoriteMeals.removeAt(existingIndex);
-      });
-    } else {
-      setState(() {
-        _favoriteMeals.add(
-          DUMMY_MEALS.firstWhere((meal) => meal.id == mealId),
-        );
-      });
-    }
-  }
+  // void _toggleFavorite(String mealId) {
+  //   final existingIndex =
+  //       _favoriteMeals.indexWhere((meal) => meal.id == mealId);
+  //   if (existingIndex >= 0) {
+  //     setState(() {
+  //       _favoriteMeals.removeAt(existingIndex);
+  //     });
+  //   } else {
+  //     setState(() {
+  //       _favoriteMeals.add(
+  //         DUMMY_MEALS.firstWhere((meal) => meal.id == mealId),
+  //       );
+  //     });
+  //   }
+  // }
 
-  bool _isMealFavorite(String id) {
-    return _favoriteMeals.any((meal) => meal.id == id);
-  }
+  // bool _isMealFavorite(String id) {
+  //   return _favoriteMeals.any((meal) => meal.id == id);
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'DeliMeals',
-      theme: ThemeData(
-        primarySwatch: Colors.pink,
-        accentColor: Colors.amber,
-        canvasColor: Color.fromRGBO(255, 254, 229, 1),
-        fontFamily: 'Raleway',
-        textTheme: ThemeData.light().textTheme.copyWith(
-            bodyText1: TextStyle(
-              color: Color.fromRGBO(20, 51, 51, 1),
-            ),
-            bodyText2: TextStyle(
-              color: Color.fromRGBO(20, 51, 51, 1),
-            ),
-            headline1: TextStyle(
-              fontSize: 20,
-              fontFamily: 'RobotoCondensed',
-              fontWeight: FontWeight.bold,
-            )),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => Meals(),
+        ),
+        ChangeNotifierProvider(
+          create: (ctx) => Categories(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'DeliMeals',
+        theme: ThemeData(
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          fontFamily: 'Raleway',
+          textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              bodyText2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              headline1: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.bold,
+              )),
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.pink)
+              .copyWith(secondary: Colors.amber),
+        ),
+        home: CategoriesScreen(),
+        routes: {
+          // '/': (ctx) => TabsScreen(_favoriteMeals),
+          // CategoryMealScreen.routeName: (ctx) =>
+          //     CategoryMealScreen(_availableMeal),
+          // MealDetailScreen.routeName: (ctx) =>
+          //     MealDetailScreen(_toggleFavorite, _isMealFavorite),
+          // FilterScreen.RouteName: (ctx) => FilterScreen(),
+        },
       ),
-      // home: CategoriesScreen(),
-      initialRoute: '/', // default is '/'
-      routes: {
-        '/': (ctx) => TabsScreen(_favoriteMeals),
-        CategoryMealScreen.routeName: (ctx) =>
-            CategoryMealScreen(_availableMeal),
-        MealDetailScreen.routeName: (ctx) =>
-            MealDetailScreen(_toggleFavorite, _isMealFavorite),
-        FilterScreen.RouteName: (ctx) => FilterScreen(_filters, _setFilters),
-      },
-
-      onUnknownRoute: (settings) {
-        return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
-      },
     );
   }
 }
--- food_app/lib/screens/categories_screen.dart
+++ food_app/lib/screens/categories_screen.dart
@@ -1,27 +1,52 @@
 import 'package:flutter/material.dart';
 
-import '../dummy_data.dart';
-import '../widgets/category_item.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/categories.dart';
+// import '../models/category.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      padding: const EdgeInsets.all(25),
-      children: DUMMY_CATEGORIES
-          .map(
-            (catData) => CategoryItem(
-              catData.id,
-              catData.title,
-              catData.color,
+    final categoriesData = Provider.of<Categories>(context);
+    final categoriesList = categoriesData.items;
+    return Scaffold(
+      // backgroundColor: Theme.of(context).backgroundColor,
+      appBar: AppBar(
+        title: Text("DeliMeal"),
+      ),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(25),
+        itemCount: categoriesList.length,
+        itemBuilder: (context, index) {
+          return InkWell(
+            onTap: () => {},
+            borderRadius: BorderRadius.circular(15),
+            // splashColor: Theme.of(context).primaryColor,
+            child: Container(
+              padding: const EdgeInsets.all(15),
+              child: Text(categoriesList[index].title,
+                  style: Theme.of(context).textTheme.headline1),
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    categoriesList[index].color.withOpacity(0.7),
+                    categoriesList[index].color
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+                borderRadius: BorderRadius.circular(15),
+              ),
             ),
-          )
-          .toList(),
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+          );
+        },
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
       ),
     );
   }
